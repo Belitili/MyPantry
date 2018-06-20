2980f3e7c0e7bf19ea003f8145a0b021bf4ae8e7
Marie <maria.verdonck@student.ucll.be>
2018-06-20 19:03:17 +0200

    filter by location added

--- app/src/main/java/com/example/marie/mypantry/MainActivity.java
+++ app/src/main/java/com/example/marie/mypantry/MainActivity.java
@@ -7,7 +7,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -45,25 +44,13 @@ public class MainActivity extends AppCompatActivity {
                 this.addProductDialog();
                 return true;
             case R.id.action_search_products:
-                final EditText searchProductEditText = new EditText(this);
-                AlertDialog dialogSearch = new AlertDialog.Builder(this)
-                        .setTitle(this.getString(R.string.search_products))
-                        .setView(searchProductEditText)
-                        .setPositiveButton(this.getString(R.string.search), new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogSearch, int which) {
-                                String searchString = String.valueOf(searchProductEditText.getText());
-                                Log.d(TAG, "Search for: " + searchString);
-                                //search in local db and print list
-                                printSearchList(searchString);
-                            }
-                        })
-                        .setNegativeButton(this.getString(R.string.cancel), null)
-                        .create();
-                dialogSearch.show();
+                this.searchProductsDialog();
                 return true;
             case R.id.action_show_all:
                 this.printAllList();
+                return true;
+            case R.id.action_filter_location:
+                this.locationFilterDialog();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -122,6 +109,50 @@ public class MainActivity extends AppCompatActivity {
         return;
     }
 
+    private void searchProductsDialog() {
+        final EditText searchProductEditText = new EditText(this);
+        AlertDialog dialogSearch = new AlertDialog.Builder(this)
+                .setTitle(this.getString(R.string.search_products))
+                .setView(searchProductEditText)
+                .setPositiveButton(this.getString(R.string.search), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogSearch, int which) {
+                        String searchString = String.valueOf(searchProductEditText.getText());
+                        Log.d(TAG, "Search for: " + searchString);
+                        //search in local db and print list
+                        printSearchList(searchString);
+                    }
+                })
+                .setNegativeButton(this.getString(R.string.cancel), null)
+                .create();
+        dialogSearch.show();
+        return;
+    }
+
+    private void locationFilterDialog() {
+        final Spinner locationSpinner = new Spinner(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.locations_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        locationSpinner.setAdapter(adapter);
+
+        AlertDialog dialogSearch = new AlertDialog.Builder(this)
+                .setTitle(this.getString(R.string.filter_location))
+                .setView(locationSpinner)
+                .setPositiveButton(this.getString(R.string.search), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogSearch, int which) {
+                        String locationString = locationSpinner.getSelectedItem().toString();
+                        Log.d(TAG, "Filter by location: " + locationString);
+                        //search in local db and print list
+                        printLocationFilteredList(locationString);
+                    }
+                })
+                .setNegativeButton(this.getString(R.string.cancel), null)
+                .create();
+        dialogSearch.show();
+        return;
+    }
 
     //print list of all products and show menu
     @Override
@@ -140,6 +171,28 @@ public class MainActivity extends AppCompatActivity {
         String whereClause = ProductDBContract.ProductEntry.COLUMN_NAME_PRODUCT +  " LIKE ?";
         searchString = "%"+searchString+"%"; //case insensitive, can be in middle of string
         String[] whereArgs = new String[]{searchString};
+        Cursor cursor = db.query(ProductDBContract.ProductEntry.TABLE_NAME,
+                new String[]{ProductDBContract.ProductEntry._ID,
+                        ProductDBContract.ProductEntry.COLUMN_NAME_PRODUCT,
+                        ProductDBContract.ProductEntry.COLUMN_NAME_AMOUNT,
+                        ProductDBContract.ProductEntry.COLUMN_NAME_LOCATION},
+                whereClause, whereArgs, null, null, null);
+        Log.d(TAG, "cursor: " + cursor);
+
+        this.printFromQueryList(cursor);
+
+        cursor.close();
+        Log.d(TAG, "ProductList updated");
+        db.close();
+        return;
+    }
+
+    //print list based on location filter
+    private void printLocationFilteredList(String locationString) {
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        String whereClause = ProductDBContract.ProductEntry.COLUMN_NAME_LOCATION +  " = ?";
+        String[] whereArgs = new String[]{locationString};
         Cursor cursor = db.query(ProductDBContract.ProductEntry.TABLE_NAME,
                 new String[]{ProductDBContract.ProductEntry._ID,
                         ProductDBContract.ProductEntry.COLUMN_NAME_PRODUCT,
